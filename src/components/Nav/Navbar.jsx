3c9d26146f1303c89787cff51a3bb21cf64ac51a
airaarima <arimacoelhoaira@gmail.com>
2024-04-15 17:58:33 -0300

    raw: pastas components e img

---
+++ src/components/Nav/Navbar.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+import styles from "./Navbar.module.css"
+import {getImgPath} from '../../utils'
+
+const Navbar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+    
+  return (
+    <nav className={styles.navbar}>
+        <a className={styles.title}href="#inicio">Josiane Schlosser</a>
+        <div className={styles.menu}>
+          <img className={styles.menuBtn} src={menuOpen ? getImgPath("nav/closeIcon.png") : getImgPath("nav/menuIcon.png")} alt="menu-button" onClick={() => setMenuOpen(!menuOpen)}/>
+            <ul className={`${styles.menuItens} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}>
+                <li><a href="#sobre" className={styles.menuItem}>Home</a></li>
+                <li><a href="#tecnologias" className={styles.menuItem}>Sobre</a></li>
+                <li><a href="#projetos" className={styles.menuItem}>Serviços</a></li>
+                <li><a href="#projetos" className={styles.menuItem}>Orçamento</a></li>
+                <li><a href="#contato" className={styles.menuItem}>Contato</a></li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
