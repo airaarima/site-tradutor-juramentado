dbef3db9cdc1e991099f1a52144601daa857d251
airaarima <arimacoelhoaira@gmail.com>
2024-04-19 11:02:22 -0300

    feat: seção home de apresentação

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import styles from "./App.module.css"
 import Navbar from './components/Nav/Navbar.jsx'
+import Home from './components/Home/Home.jsx'
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar/>
+      <Home/>
     </div>
   )
 }
