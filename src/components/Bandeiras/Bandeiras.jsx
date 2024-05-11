32b26b01b25857a3ab6b1fc93ddfed393a449ffb
airaarima <arimacoelhoaira@gmail.com>
2024-05-11 13:19:09 -0300

    refactor: ajuste de responsividade

--- src/components/Bandeiras/Bandeiras.jsx
+++ src/components/Bandeiras/Bandeiras.jsx
@@ -7,9 +7,13 @@ const Bandeiras = () => {
     <section className={styles.container}>
       <h2 className={styles.title}>Josiane Schlosser</h2>
       <p className={styles.description}>Tradutora Pública - Juramentada em espanhol</p>
-      <div className={styles.content}>
+      <div className={styles.contentMaior}>
         <img src={getImgPath("bandeiras/espanha.png")} alt="" className={styles.contentImg}/>
         <img src={getImgPath("bandeiras/brasil.png")} alt="" className={styles.contentImg}/>
+      </div>
+      <div className={styles.contentMenor}>
+        <img src={getImgPath("bandeiras/espanha-menor.png")} alt="" className={styles.contentImg}/>
+        <img src={getImgPath("bandeiras/brasil-menor.png")} alt="" className={styles.contentImg}/>
       </div>
 
     </section>
