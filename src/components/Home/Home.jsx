6972b6dd053a180ea4302b560f86a9488e78f901
airaarima <arimacoelhoaira@gmail.com>
2024-05-24 17:37:12 -0300

    refactor: atualização de textos de apresentação

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <section className={styles.container} id="home">
         <div className={styles.content}>
-            <h1 className={styles.title}>Olá, me chamo Josiane!</h1>
+            <h1 className={styles.title}>Olá, meu nome é Josiane!</h1>
             <p className={styles.description}>Comecei a traduzir em 2008 e em 2010 fiz o concurso para tradutora juramentada. Fui aprovada e desde então atuo na área realizando traduções do espanhol para o português, e versões do português para o espanhol.</p>
             <a href="#contato" className={styles.contatoBtn}>Contate-me</a>
         </div>
