56a91df263bc330f20140f5ba620c0473820a795
airaarima <arimacoelhoaira@gmail.com>
2024-06-18 19:07:26 -0300

    chore(home): atualização do texto de apresentação

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <section className={styles.container} id="home">
         <div className={styles.content}>
             <h1 className={styles.title}>Olá, meu nome é Josiane!</h1>
-            <p className={styles.description}>Comecei a traduzir em 2008 e em 2010 fiz o concurso para tradutora juramentada. Fui aprovada e desde então atuo na área realizando traduções do espanhol para o português, e versões do português para o espanhol.</p>
+            <p className={styles.description}>Sou licenciada em Espanhol pela UFSM, fui professora de Língua Espanhola por 13 anos e já residi em Madri por 4 anos. Em 2010 fui aprovada no concurso para tradutora juramentada e desde então atuo na área realizando traduções do espanhol para o português, e versões do português para o espanhol.</p>
             <a href="#contato" className={styles.contatoBtn}>Contate-me</a>
         </div>
         <div className={styles.divImg}>
