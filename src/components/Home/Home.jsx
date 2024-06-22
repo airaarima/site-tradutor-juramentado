c91dfbd4e999d52d44be045189bd44f2de9bdbdc
airaarima <arimacoelhoaira@gmail.com>
2024-06-22 15:37:08 -0300

    refactor: botão de redirecionamento para whatsapp

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <div className={styles.content}>
             <h1 className={styles.title}>Olá, meu nome é Josiane!</h1>
             <p className={styles.description}>Sou licenciada em Espanhol pela UFSM, fui professora de Língua Espanhola por 13 anos e já residi em Madri por 4 anos. Em 2010 fui aprovada no concurso para tradutora juramentada e desde então atuo na área realizando traduções do espanhol para o português, e versões do português para o espanhol.</p>
-            <a href="#contato" className={styles.contatoBtn}>Contate-me</a>
+            <a href="https://wa.me/5555996578731?text=Olá, Josiane. Gostaria de fazer um orçamento para a tradução/versão de um documento." target='_blank' className={styles.contatoBtn}>Contate-me</a>
         </div>
         <div className={styles.divImg}>
           <img src={getImgPath("pessoal/perfil2.jpg")} alt="Imagem pessoal" className={styles.pessoalImg}/>
